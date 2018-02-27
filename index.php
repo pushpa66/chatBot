5976877d552ec2aa5507976210862f0c6a4c9c57
pushpakumara.14@cse.mrt.ac.lk <pushpakumara.14@cse.mrt.ac.lk>
2018-02-27 15:55:55 +0530

    test

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 $accessToken = "EAACVyPw6FJoBAFAcdTEPDrruWBykdeLgwivYwH1GCGWSZCQETLekDP63HJlQ5EeExDaRMNI5UczQaHaa4C25aeWFEMNnF9mHc6BOq3AcjawGEQZBDSHCZCH43YH2k8YneWXxgxDrZB5NZAUYWfbBZBASydcdYevfZCtaRfKsZCJq5S7UsSMrITsZB";
-var_dump($accessToken);
 if (isset($_GET['hub_mode']) && isset($_GET['hub_challenge']) && isset($_GET['hub_verify_token'])){
     if($_GET['hub_verify_token'] == "abcd1234") {
         echo $_GET['hub_challenge'];
